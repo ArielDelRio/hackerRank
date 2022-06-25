2a5f4a3053e09d3a279746e945ce35c75dd57c2f
ArieldRio <arieldelrioviamonte@gmail.com>
2022-06-25 16:05:31 -0400

    Add CountingSort problem and solution

---
+++ challenges/CountingSort1/CountingSort1.ts
@@ -0,0 +1,9 @@
+function countingSort(arr: number[]): number[] {
+  const res: number[] = new Array(100).fill(0);
+
+  arr.forEach((value) => (res[value] += 1));
+
+  return res;
+}
+
+export default countingSort;

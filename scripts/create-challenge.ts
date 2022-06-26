68a0a13fd6ca3bae8aa31a3ffe9f8fe3e23d583d
ArieldRio <arieldelrioviamonte@gmail.com>
2022-06-26 17:35:53 -0400

    Update solutions path on create-challenge script

--- scripts/create-challenge.ts
+++ scripts/create-challenge.ts
@@ -2,16 +2,23 @@ import fs from "fs";
 
 const args = process.argv;
 const challengeName = args[3] || "NewChallenge";
+const user = args[4] || "Anonymous";
 const challengePath = `./challenges/${challengeName}`;
+const solutionsPath = `./solutions/${user}/${challengeName}`;
 
 console.log("creating folder...");
+if (!fs.existsSync(solutionsPath)) {
+  fs.mkdirSync(solutionsPath);
+  console.log(`${challengeName} solution folder is created successfully`);
+}
+
 if (!fs.existsSync(challengePath)) {
   fs.mkdirSync(challengePath);
-  console.log(`${challengeName} folder is created successfully`);
+  console.log(`${challengeName} challenge folder is created successfully`);
 }
 
 fs.writeFile(
-  `${challengePath}/${challengeName}.ts`,
+  `${solutionsPath}/${challengeName}.ts`,
   "// Write your Code here...",
   function (err) {
     if (err) throw err;
@@ -20,7 +27,7 @@ fs.writeFile(
 );
 
 fs.writeFile(
-  `${challengePath}/${challengeName}.test.ts`,
+  `${solutionsPath}/${challengeName}.test.ts`,
   "// Write your test here...",
   function (err) {
     if (err) throw err;
